0f8e14ba3c22841d339571d5ce12d923a52fe217
rai <mj82xx@gmail.com>
2022-08-16 16:22:08 +0900

    ci: automatic pyproject version update

---
+++ .github/workflows/version.py
@@ -0,0 +1,22 @@
+import sys
+import tomlkit
+from pprint import pprint
+import re
+
+
+def main():
+    pyproject_path = sys.argv[1]
+    version = re.search(r'\d+\.\d+\.\d+', sys.argv[2]).group()
+    with open(pyproject_path, 'r') as f:
+        pyproject_obj = tomlkit.parse(f.read())
+    # ! LOG
+    print(
+        f"chenged the pyproject version from {pyproject_obj['tool']['poetry']['version']} to {version}"
+    )
+    pyproject_obj['tool']['poetry']['version'] = version
+    with open(pyproject_path, 'w') as f:
+        tomlkit.dump(pyproject_obj, f)
+
+
+if __name__ == '__main__':
+    main()
