41d83d60d7f85e3bd365894218cef5aed08f0deb
rai <mj82xx@gmail.com>
2023-06-09 08:53:56 +0900

    refactor: format code

--- src/pandoc_pdf/main.py
+++ src/pandoc_pdf/main.py
@@ -1,58 +1,47 @@
-from asyncio import subprocess
-from pandoc_pdf_utils.functions import \
-    init_config, init_setting, init_cache, generate_command_docker, generate_command_pandoc
+from pandoc_pdf_utils.functions import (
+    init_config,
+    init_setting,
+    init_cache,
+    generate_command_docker,
+    generate_command_pandoc,
+)
 from pandoc_pdf_utils.env import CACHE_DIR, CONFIG_DIR
 from pathlib import Path
 import subprocess
 import click
 import yaml
 from copy import deepcopy
-from pprint import pprint
 
 init_config()
-with open(CONFIG_DIR / 'defaults.yml') as f:
+with open(CONFIG_DIR / "defaults.yml") as f:
     presets = [key_i for key_i in yaml.safe_load(f)]
 
 
 @click.command()
+@click.option("--debug", is_flag=True)
+@click.option("-D", "--docker", type=str)
 @click.option(
-    '--debug',
-    is_flag=True
-)
-@click.option(
-    '-D', '--docker',
-    type=str
-)
-@click.option(
-    '-v', '--volume',
+    "-v",
+    "--volume",
     type=str,
     multiple=True,
 )
+@click.option("-V", "--variable", type=str, multiple=True)
+@click.option("-M", "--metadata", type=str, multiple=True)
+@click.option("-p", "--preset", type=click.Choice(presets), default="latex")
 @click.option(
-    '-V', '--variable',
-    type=str,
-    multiple=True
-)
-@click.option(
-    '-M', '--metadata',
-    type=str,
-    multiple=True
-)
-@click.option(
-    '-p', '--preset',
-    type=click.Choice(presets),
-    default="latex"
-)
-@click.option(
-    '-o', '--output',
+    "-o",
+    "--output",
     type=click.Path(path_type=Path),
-    default='NULL',
+    default="NULL",
 )
 @click.argument(
-    'input_file',
+    "input_file",
     type=click.Path(exists=True, path_type=Path),
 )
-def pandoc_pdf(input_file: Path, debug: bool, docker, volume, metadata, variable, preset, output):
+def pandoc_pdf(
+    input_file: Path, debug: bool, docker, volume, metadata, variable, preset, output
+):
     """Command to generate pdf easily with pandoc."""
     output_file = deepcopy(output)
     volumes = deepcopy(volume)
@@ -62,37 +51,43 @@ def pandoc_pdf(input_file: Path, debug: bool, docker, volume, metadata, variable
     del volume
     del variable
     del metadata
-    if str(output_file) == 'NULL':
+    if str(output_file) == "NULL":
         output_file = Path(f"{input_file.parent / input_file.stem}.pdf")
     init_cache()
     setting_obj = init_setting(docker, volumes)
-    if setting_obj['docker']['use_docker']:
-        defaults_file = Path(f'/cache/defaults_{preset}.yml')
+    if setting_obj["docker"]["use_docker"]:
+        defaults_file = Path(f"/cache/defaults_{preset}.yml")
     else:
-        defaults_file = CACHE_DIR / f'defaults_{preset}.yml'
+        defaults_file = CACHE_DIR / f"defaults_{preset}.yml"
 
     # * ---Execute command
     args_docker = generate_command_docker(setting_obj)
     args_pandoc = generate_command_pandoc(
-        setting_obj, defaults_file, input_file, output_file, preset, variables, metadatas
+        setting_obj,
+        defaults_file,
+        input_file,
+        output_file,
+        preset,
+        variables,
+        metadatas,
     )
-    if setting_obj['docker']['use_docker'] == True:
-        args = ' '.join(args_docker) + f" \"{' '.join(args_pandoc)}\""
+    if setting_obj["docker"]["use_docker"] == True:
+        args = " ".join(args_docker) + f" \"{' '.join(args_pandoc)}\""
     else:
-        args = ' '.join(args_pandoc)
+        args = " ".join(args_pandoc)
     result = subprocess.run(args, shell=True)
-    result_status = 'Succeeded' if result.returncode == 0 else 'Failed'
-    result_color = '' if result.returncode == 0 else 'red'
+    result_status = "Succeeded" if result.returncode == 0 else "Failed"
+    result_color = "" if result.returncode == 0 else "red"
     click.secho(
-        f'{result_status} to generate {str(output_file)} from {str(input_file)} by {preset}.',
+        f"{result_status} to generate {str(output_file)} from {str(input_file)} by {preset}.",
         fg=result_color,
-        bold=True
+        bold=True,
     )
 
     if debug:
-        click.secho('\n---< DEBUG >---', fg='red')
-        click.secho('Executed command:')
-        click.secho(f'  {result.args}', bold=True)
+        click.secho("\n---< DEBUG >---", fg="red")
+        click.secho("Executed command:")
+        click.secho(f"  {result.args}", bold=True)
 
     if result.returncode == 0:
         return 0
